e81955545ed6ccab0306f3626c4702a13bff1cc6
Joshua de la Bruere <j.l.delabruere@gmail.com>
2021-12-09 10:19:28 -0600

    Update day 9.R

--- aoc2021/code/day 9.R
+++ aoc2021/code/day 9.R
@@ -8,10 +8,10 @@ find_local_minima <- function(mat){
   cols <- ncol(mat)
 
   combined <-
-  rbind(input[1:(rows-1),] < input[2:rows,], rep(TRUE,rows)) &
-  rbind(rep(TRUE,rows), input[2:rows,] < input[1:(rows-1),]) &
-  cbind(input[,1:(cols-1)] < input[,2:cols], rep(TRUE,cols)) &
-  cbind(rep(TRUE,cols), input[,2:cols] < input[,1:(cols-1)])
+  rbind(input[1:(rows-1),] < input[2:rows,], TRUE) &
+  rbind(TRUE, input[2:rows,] < input[1:(rows-1),]) &
+  cbind(input[,1:(cols-1)] < input[,2:cols], cols) &
+  cbind(cols, input[,2:cols] < input[,1:(cols-1)])
 
 
 }
@@ -28,26 +28,26 @@ find_basins <- function(mat){
   cols <- ncol(mat)
 
   minima <- find_local_minima(mat)
-  mat[] <- ifelse(mat < 9, NA, Inf)
+  mat[] <- ifelse(mat < max(mat), NA, Inf)
   mat[minima] <- 1:sum(minima)
 
   na_to_finite_neighbor <- function(x,y){
     ifelse(
-      is.infinite(x), x,
-      ifelse(
-        !is.na(x), x,
+      (!is.na(x) | is.infinite(x)), x,
         ifelse(
           !is.infinite(y), y, x  )
       )
-    )
+
   }
 
   while (anyNA(mat)) {
     mat <- mat |>
       na_to_finite_neighbor(cbind(mat, NA)[,-1] )  |>#right
-      na_to_finite_neighbor(rbind(mat, NA)[-1,] )    |> #down
       na_to_finite_neighbor(cbind(NA, mat[,-cols]))  |>#left
-      na_to_finite_neighbor(rbind(NA, mat[-rows,]))  #up
+      na_to_finite_neighbor(rbind(NA, mat[-rows,]))  |> #up
+      na_to_finite_neighbor(rbind(mat, NA)[-1,] )    #down
+
+
   }
 
 
